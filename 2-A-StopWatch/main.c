b396953aa982d87aa18823e650f2176abe1b5884
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-18 21:38:22 +0000

    Added seconds. minute and hours to task scheduler
    - copied hundredth function for seconds minutes and hours
    - performed validation
    - added switch to hh;mm;ss after 1 hour has passed

--- 2-A-StopWatch/main.c
+++ 2-A-StopWatch/main.c
@@ -17,37 +17,73 @@
 #include "DE1Soc_SevenSeg/DE1Soc_SevenSeg.h"
 
 // Define new data type for a functions which takes an int and returns an int
-typedef unsigned int (*TaskFunction)(unsigned int);
+typedef unsigned int (*TaskFunction)(unsigned int, unsigned int);
 
 // Declare functions
-unsigned int update_hundredths (unsigned int hundredths) {
-	if (hundredths > 99) hundredths = 1;
-	else hundredths = hundredths + 1;
-	DE1SoC_SevenSeg_SetDoubleDec(0, hundredths);
+unsigned int update_hundredths (unsigned int hours, unsigned int hundredths) {
+	if (hundredths < 99) hundredths = hundredths + 1;
+	else hundredths = 0;
+
+	if (hours < 1) DE1SoC_SevenSeg_SetDoubleDec(0, hundredths);
 
 	return hundredths;
 }
 
+unsigned int update_seconds (unsigned int hours, unsigned int seconds) {
+	if (seconds < 59) seconds = seconds + 1;
+	else seconds = 0;
+
+	if (hours < 1) DE1SoC_SevenSeg_SetDoubleDec(2, seconds);
+	else DE1SoC_SevenSeg_SetDoubleDec(0, seconds);
+
+	return seconds;
+}
+
+unsigned int update_minutes (unsigned int hours, unsigned int minutes) {
+	if (minutes < 59) minutes = minutes + 1;
+	else minutes = 0;
+
+	if (hours < 1) DE1SoC_SevenSeg_SetDoubleDec(4, minutes);
+	else DE1SoC_SevenSeg_SetDoubleDec(2, minutes);
+
+	return minutes;
+}
+
+unsigned int update_hours (unsigned int hours, unsigned int unused) {
+	if (hours < 99) hours = hours + 1;
+	else hours = 0;
+
+	if (hours > 0) DE1SoC_SevenSeg_SetDoubleDec(4, hours);
+
+	return hours;
+}
+
 main() {
+
+
 	// local variables
-	const unsigned int taskCount = 1;
+	const unsigned int taskCount = 4;
 	unsigned int taskID = 0;
 	unsigned int taskLastTime[taskCount];
-	unsigned int taskInterval[taskCount] = {2250000};
-	TaskFunction taskFunctions[taskCount] = {&update_hundredths};
+	unsigned int taskInterval[taskCount] = {10000, 1000000, 60000000, 3600000000};
+	TaskFunction taskFunctions[taskCount] = {&update_hundredths, &update_seconds, &update_minutes, &update_hours};
 
-	// current timer storage variable
-	unsigned int time[taskCount];
-
-	// set intial task times
-	for (taskID = 0; taskID < taskCount; taskID++) {
-		taskLastTime[taskID] = Timer_readTimer();      //All tasks start now
-	}
+	unsigned int time[taskCount] = {0, 0, 0, 0};
 
 	// configure ARM private timer
 	Timer_initialise(0xFFFEC600);
 	Timer_setLoadValue(0xFFFFFFFF);
-	Timer_setControl(0, 0, 1, 1);
+	Timer_setControl(4, 0, 1, 1);
+
+	for (taskID = 0; taskID < taskCount; taskID++) {
+		taskLastTime[taskID] = Timer_readTimer();      //All tasks start now
+	}
+
+	// set intial task times
+//	taskLastTime[0] = Timer_readTimer();		//All tasks start now
+//	for (taskID = 1; taskID < taskCount+1; taskID++) {
+//		taskLastTime[taskID] = 0;							//All timer storage start at 0
+//	}
 
 	while(1) {
 		// Read the current time
@@ -55,8 +91,7 @@ main() {
 
 		for (taskID = 0; taskID < taskCount; taskID++) {
 			if ((taskLastTime[taskID] - currentTimerValue) >= taskInterval[taskID]) {
-				time[taskID] = taskFunctions[taskID](time[taskID]);
-
+				time[taskID] = taskFunctions[taskID](time[3], time[taskID]);
 				taskLastTime[taskID] = taskLastTime[taskID] - taskInterval[taskID];
 			}
 		}
