8334b2e707d0b6b89cbbf90d9fd0e023f2e1234c
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-19 19:46:11 +0000

    Added servo motor metronome function
    - added servomotor metronome function to the code
    - functionalised the timer and servo initialisation

--- 2-A-StopWatch/main.c
+++ 2-A-StopWatch/main.c
@@ -15,6 +15,7 @@
 #include "HPS_Watchdog/HPS_Watchdog.h"
 #include "HPS_PrivateTimer/HPS_PrivateTimer.h"
 #include "DE1Soc_SevenSeg/DE1Soc_SevenSeg.h"
+#include "DE1SoC_Servo/DE1SoC_Servo.h"
 
 volatile unsigned int *key_ptr = (unsigned int *)0xFF200050; // KEYS 0-3 (push buttons)
 volatile unsigned int *LEDR_ptr = (unsigned int *)0xFF200000; // LEDS 0-9
@@ -23,7 +24,9 @@ volatile unsigned int *LEDR_ptr = (unsigned int *)0xFF200000; // LEDS 0-9
 // Define new data type for a functions which takes an int and returns an int
 typedef unsigned int (*TaskFunction)(unsigned int, unsigned int);
 
-// Declare functions
+//
+// Declare task scheduler functions
+//
 unsigned int update_hundredths (unsigned int hours, unsigned int hundredths) {
 	if (hundredths < 99) hundredths = hundredths + 1;
 	else hundredths = 0;
@@ -34,11 +37,19 @@ unsigned int update_hundredths (unsigned int hours, unsigned int hundredths) {
 }
 
 unsigned int update_seconds (unsigned int hours, unsigned int seconds) {
+	signed char position; // calibration performed manually as calibration function does not work
+	signed char calibration = 20;
+
 	if (seconds < 59) seconds = seconds + 1;
 	else seconds = 0;
 
 	if (hours < 1) DE1SoC_SevenSeg_SetDoubleDec(2, seconds);
 	else DE1SoC_SevenSeg_SetDoubleDec(0, seconds);
+
+	if (seconds % 2) position = 64 + calibration;
+	else position = -64 + calibration;
+
+	Servo_pulseWidth(0, position);
 
 	return seconds;
 }
@@ -62,9 +73,31 @@ unsigned int update_hours (unsigned int hours, unsigned int unused) {
 	return hours;
 }
 
+//
+// Declare other functions
+//
+void configure_privateTimer () {
+	Timer_initialise(0xFFFEC600);	// set private timer base address
+	Timer_setLoadValue(0xFFFFFFFF);	// load maximum value
+	Timer_setControl(224, 0, 1, 0);	// timer intialised to disabled mode
+
+}
+
+void configure_servoDrivers () {
+	//signed char calibrate = -100;
+	Servo_initialise(0xFF2000C0);
+	Servo_enable(0, true);
+	Servo_pulseWidthRange(0, true);
+
+	// Servo calibrate does not seem to work
+	//Servo_calibrate(0, calibrate);
+
+}
 
 void reset_stopWatch (unsigned int* time, unsigned int* taskLastTime, unsigned int taskCount) {
 	unsigned int taskID;
+
+	Timer_setControl(224, 0, 1, 0);
 	for (taskID = 0; taskID < taskCount; taskID++) {
 		time[taskID] = 0;
 		taskLastTime[taskID] = Timer_readTimer();      //All tasks start now
@@ -76,25 +109,25 @@ void reset_stopWatch (unsigned int* time, unsigned int* taskLastTime, unsigned i
 }
 
 main() {
-	// local variables
+	// taskscheduler variables
 	const unsigned int taskCount = 4;
 	unsigned int taskID = 0;
 	unsigned int taskLastTime[taskCount];
-	unsigned int taskInterval[taskCount] = {10000, 1000000, 60000000, 3600000000};
+	long long taskInterval[taskCount] = {10000, 1000000, 60000000, 3600000000};
 	TaskFunction taskFunctions[taskCount] = {&update_hundredths, &update_seconds, &update_minutes, &update_hours};
 
 	unsigned int time[taskCount] = {0};
 	unsigned int currentTimerValue;
 
-	// configure ARM private timer
-	Timer_initialise(0xFFFEC600);
-	Timer_setLoadValue(0xFFFFFFFF);
-	Timer_setControl(224, 0, 1, 1);
+	//configure drivers
+	configure_privateTimer();
+	configure_servoDrivers();
 
-	// All tasks start now
+	// Begin task timer now
 	reset_stopWatch(time, taskLastTime, taskCount);
 
 	while(1) {
+		// poll keys to check for user inputs
 		if(*key_ptr & 0x01) Timer_setControl(224, 0, 1, 1);						//enable timer when start pressed
 		else if(*key_ptr & 0x02) Timer_setControl(224, 0, 1, 0);				//disable timer when stop pressed
 		else if(*key_ptr &0x04) reset_stopWatch(time, taskLastTime, taskCount);	//reset time when rest pressed
