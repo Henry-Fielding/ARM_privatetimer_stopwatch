32d9495fef0558efc87305a0516963e9876401cf
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-22 15:41:27 +0000

    Added stopWatchTimer source file to project
    - added stopwatch timer source file to control timer functions

---
+++ 2-A-stopWatch/stopWatchTimer.c
@@ -0,0 +1,8 @@
+/*
+ * stopWatchTimer.c
+ *
+ *  Created on: 22 Mar 2021
+ *      Author: Henry
+ */
+
+
---
+++ 2-A-stopWatch/stopWatchTimer.h
@@ -0,0 +1,13 @@
+/*
+ * stopWatchTimer.h
+ *
+ *  Created on: 22 Mar 2021
+ *      Author: Henry
+ */
+
+#ifndef STOPWATCHTIMER_H_
+#define STOPWATCHTIMER_H_
+
+
+
+#endif /* STOPWATCHTIMER_H_ */
