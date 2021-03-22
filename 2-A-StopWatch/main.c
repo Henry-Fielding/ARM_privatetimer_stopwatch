6f2745dc96193ba75853658e6d25a0182f1bef0b
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-03-22 19:14:19 +0000

    Move statemachine to a seperate function
    - created seperate function for stopwatch statemachine
    - created seperate function for updating the metronome

--- 2-A-StopWatch/main.c
+++ 2-A-StopWatch/main.c
@@ -14,111 +14,39 @@
 #include "main.h"
 
 main() {
-	// taskscheduler variables
-	int state = 0;
-	int splitMode = 0;
-
+	// declare task scheduler variables
 	const unsigned int taskCount = 4;
 	const unsigned int period = 10000;
 	const unsigned int taskInterval[taskCount] = {period, 100*period, 6000*period, 360000*period};
 	unsigned int taskLastTime[taskCount];
 	TaskFunction taskFunctions[taskCount] = {&update_hundredths, &update_seconds, &update_minutes, &update_hours};
 
+	// declare stopwatch variables
 	unsigned int time[taskCount] = {0};
 	unsigned int splitTime[taskCount] = {0};
 
-	#define RESET_STATE 0
-	#define STOP_STATE 1
-	#define START_STATE 2
-	#define SPLIT_STATE 3
+	// declare state variables
+	unsigned int state = RESET_STATE;
+	unsigned int splitMode = CUMULATIVE_SPLIT;
 
-	#define CUMMULATIVE_SPLIT 0
-	#define LAP_SPLIT 1
 
 	//configure drivers
 	configure_privateTimer();
 	configure_servoDrivers();
 
 	// Begin task timer now
-	stopWatchTimer_resetTimer(time, taskLastTime, taskCount);
+	//stopWatchTimer_resetTimer(time, taskLastTime, taskCount);
 
 	while(1) {
 
 		// update the stopwatch timer value
 		stopWatchTimer_updateTimer (taskFunctions, time, taskLastTime, taskInterval, taskCount);
 
-		// update stopwatch mode
-		switch (state) {
-			case RESET_STATE :
-				Timer_setControl(224, 0, 1, 0);
-				stopWatchTimer_resetTimer(time, taskLastTime, taskCount);
+		// update stopwatch output mode
+		stopWatch_updateStateMachine (taskFunctions, &state, &splitMode, time, splitTime, taskLastTime, taskInterval, taskCount);
 
-				display_stopWatch(time);
-
-				state = STOP_STATE;
-			break;
-
-			case STOP_STATE :
-				Timer_setControl(224, 0, 1, 0);
-				display_stopWatch(time);
-				*LEDR_ptr |= 0x001;
-
-				if (*key_edge_ptr & 0x02) {
-					update_split_mode_stopWatch(&splitMode);
-				}
-				// change state conditions
-				if(*key_edge_ptr & 0x01) {
-					state = START_STATE;
-				} else if(*key_edge_ptr & 0x04) {
-					state = RESET_STATE;
-				} else {
-					state = STOP_STATE;
-				}
-			break;
-
-			case START_STATE :
-				display_stopWatch(time);
-				Timer_setControl(224, 0, 1, 1);
-				*LEDR_ptr &= ~0x009;
-
-				// change state conditions
-				if(*key_edge_ptr & 0x01) {
-					state = STOP_STATE;
-				} else if(*key_edge_ptr & 0x04) {
-					stopWatchTimer_splitTimer(time, splitTime, taskCount);
-					if (splitMode == LAP_SPLIT) {
-						stopWatchTimer_resetTimer(time, taskLastTime, taskCount);
-					}
-					state = SPLIT_STATE;
-				} else {
-					state = START_STATE;
-				}
-
-			break;
-
-			case SPLIT_STATE :
-				display_stopWatch(splitTime);
-				Timer_setControl(224, 0, 1, 1);
-				*LEDR_ptr |= 0x008;
-
-				// change state conditions
-				if(*key_edge_ptr & 0x04) {
-					state = START_STATE;
-				} else {
-					state = SPLIT_STATE;
-				}
-			break;
-
-		}
-
-
-		clear_inputs();
-
-
-
-
-
-
+		// clear user button inputs
+		//stopWatch_clearInputs();
 
 		// Make sure we clear the private timer interrupt flag if it is set
 		if (Timer_readInterrupt() & 0x1) {
@@ -133,8 +61,6 @@ main() {
 
 }
 
-
-
 void configure_servoDrivers () {
 	signed char calibrate = 20;
 	Servo_initialise(0xFF2000C0);
@@ -145,38 +71,143 @@ void configure_servoDrivers () {
 
 }
 
-void update_split_mode_stopWatch (int* splitMode) {
-	if (*splitMode == CUMMULATIVE_SPLIT) {
-		*splitMode = LAP_SPLIT;
-		*LEDR_ptr &= ~0x004;
-		*LEDR_ptr |= 0x002;
-	} else if (*splitMode == LAP_SPLIT) {
-		*splitMode = CUMMULATIVE_SPLIT;
-		*LEDR_ptr &= ~0x002;
-		*LEDR_ptr |= 0x004;
+void stopWatch_updateStateMachine (TaskFunction* taskFunctions, unsigned int* state, unsigned int* splitMode, unsigned int* time, unsigned int* splitTime, unsigned int* taskLastTime, const unsigned int* taskInterval,  unsigned int taskCount) {
+	switch (*state) {
+		case RESET_STATE :
+			Timer_setControl(224, 0, 1, 0);
+			stopWatchTimer_resetTimer(time, taskLastTime, taskCount);
+
+			stopWatch_displayTime(time);
+			stopWatch_updateMetronome (time);
+
+			*state = STOP_STATE;
+		break;
+
+		case STOP_STATE :
+			Timer_setControl(224, 0, 1, 0);
+			stopWatch_updateSplitMode(splitMode);
+
+			stopWatch_displayTime(time);
+			stopWatch_updateMetronome (time);
+			*LEDR_ptr |= 0x001;
+
+			// change state conditions
+			if(*key_edge_ptr & 0x01) {
+				stopWatch_clearInputs();
+				*state = START_STATE;
+			} else if(*key_edge_ptr & 0x04) {
+				stopWatch_clearInputs();
+				*state = RESET_STATE;
+			} else {
+				*state = STOP_STATE;
+			}
+		break;
+
+		case START_STATE :
+			Timer_setControl(224, 0, 1, 1);
+
+			stopWatch_displayTime(time);
+			stopWatch_updateMetronome (time);
+			*LEDR_ptr &= ~0x009;
+
+			// change state conditions
+			if(*key_edge_ptr & 0x01) {
+				stopWatch_clearInputs();
+				*state = STOP_STATE;
+			} else if(*key_edge_ptr & 0x04) {
+				stopWatch_clearInputs();
+				stopWatchTimer_splitTimer(time, splitTime, taskCount);
+				if (*splitMode == LAP_SPLIT) {
+					stopWatchTimer_resetTimer(time, taskLastTime, taskCount);
+				}
+				*state = SPLIT_STATE;
+			} else {
+				*state = START_STATE;
+			}
+
+		break;
+
+		case SPLIT_STATE :
+			Timer_setControl(224, 0, 1, 1);
+
+			stopWatch_displayTime(splitTime);
+			*LEDR_ptr |= 0x008;
+
+			// change state conditions
+			if(*key_edge_ptr & 0x04) {
+				stopWatch_clearInputs ();
+				*state = START_STATE;
+			} else {
+				*state = SPLIT_STATE;
+			}
+		break;
+
+	}
+
+}
+
+void stopWatch_updateSplitMode (unsigned int* splitMode) {
+	switch (*splitMode) {
+		case CUMULATIVE_SPLIT :
+			*LEDR_ptr &= ~0x004;
+			*LEDR_ptr |= 0x002;
+
+			if (*key_edge_ptr & 0x02) {
+				*splitMode = LAP_SPLIT;
+				stopWatch_clearInputs ();
+			}
+		break;
+
+		case LAP_SPLIT :
+			*LEDR_ptr &= ~0x002;
+			*LEDR_ptr |= 0x004;
+
+			if (*key_edge_ptr & 0x02) {
+				*splitMode = CUMULATIVE_SPLIT;
+				stopWatch_clearInputs ();
+			}
+		break;
 	}
 }
 
-void display_stopWatch (unsigned int* time) {
+void stopWatch_displayTime (unsigned int* time) {
 	unsigned int hundredths = time[0];
 	unsigned int seconds = time[1];
 	unsigned int minutes = time[2];
 	unsigned int hours = time[3];
 
 	if (hours <= 0) {	// if time is less than 1 hours show MM:SS:FF, show hundredths indicator LED
-		*LEDR_ptr |= 0x200;
+		*LEDR_ptr |= 0x010;
 		DE1SoC_SevenSeg_SetDoubleDec(0, hundredths);
 		DE1SoC_SevenSeg_SetDoubleDec(2, seconds);
 		DE1SoC_SevenSeg_SetDoubleDec(4, minutes);
 	} else {	// if time is greater than 1 hours show HH:MM:SS
-		*LEDR_ptr &= ~0x200;
+		*LEDR_ptr &= ~0x010;
 		DE1SoC_SevenSeg_SetDoubleDec(0, seconds);
 		DE1SoC_SevenSeg_SetDoubleDec(2, minutes);
 		DE1SoC_SevenSeg_SetDoubleDec(4, hours);
 	}
 }
 
-void clear_inputs () {
+void stopWatch_updateMetronome (unsigned int* time) {
+	unsigned int seconds = time[1];
+	signed char position;
+	signed char calibration = 20; // calibration performed manually as calibration function does not work
+
+	if (seconds % 2) position = 64 + calibration;
+	else position = -64 + calibration;
+
+	Servo_pulseWidth(0, position);
+}
+
+void stopWatch_clearInputs () {
 	unsigned int temp = *key_edge_ptr;
 	*key_edge_ptr = temp;
 }
+
+
+
+
+
+
+
--- 2-A-stopWatch/main.h
+++ 2-A-stopWatch/main.h
@@ -26,6 +26,14 @@ volatile unsigned int *key_ptr = (unsigned int *)0xFF200050; // KEYS 0-3 (push b
 volatile unsigned int *LEDR_ptr = (unsigned int *)0xFF200000; // LEDS 0-9
 volatile unsigned int *key_edge_ptr = (unsigned int *)0xFF20005C;
 
+#define RESET_STATE 0
+#define STOP_STATE 1
+#define START_STATE 2
+#define SPLIT_STATE 3
+
+#define CUMULATIVE_SPLIT 0
+#define LAP_SPLIT 1
+
 // Define new data type for a functions which takes an int and returns an int
 //typedef unsigned int (*TaskFunction)(unsigned int);
 
@@ -35,11 +43,15 @@ volatile unsigned int *key_edge_ptr = (unsigned int *)0xFF20005C;
 
 void configure_servoDrivers (void);
 
-void update_split_mode_stopWatch (int*);
+void stopWatch_updateStateMachine (TaskFunction*, unsigned int*, unsigned int*, unsigned int*, unsigned int*, unsigned int*, const unsigned int*,  unsigned int);
 
-void display_stopWatch (unsigned int*);
+void stopWatch_updateSplitMode (unsigned int*);
 
-void clear_inputs (void);
+void stopWatch_displayTime (unsigned int*);
+
+void stopWatch_updateMetronome (unsigned int*);
+
+void stopWatch_clearInputs (void);
 
 
 #endif /* STOPWATCH_MAIN_H_ */
